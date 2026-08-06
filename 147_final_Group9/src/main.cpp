80bd70d38fc1b3449af180efb897caa59dbf34fa
Leyang Cheng <xxxmnalx@gmail.com>
2026-08-06 11:17:25 -0700

    BLE, IMU setup. Code from lab3

--- 147_final_Group9/src/main.cpp
+++ 147_final_Group9/src/main.cpp
@@ -1,11 +1,66 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <VL53L1X.h>
+#include <TFT_eSPI.h>
+#include "SparkFunLSM6DSO.h"
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include <BLE2902.h>
+
+//pins
+#define I2C_SDA_PIN   21
+#define I2C_SCL_PIN   22
+#define BUZZER_PIN    25
+#define BTN_START_PIN 26
+#define BTN_STOP_PIN  27
+
+//BLE
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+BLECharacteristic *pCharacteristic;
+
+//IMU
+LSM6DSO myIMU;
+
+
 
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  Wire.begin();
+  Wire.setClock(400000);
+  delay(100);
+
+  if (myIMU.begin() == false) {
+    Serial.println("LSM6DSO not detected");
+    while (1);
+  }
+  myIMU.initialize(BASIC_SETTINGS);
+
+  BLEDevice::init("147final_Group9");
+  BLEServer *pServer = BLEDevice::createServer();
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  pCharacteristic = pService->createCharacteristic(
+      CHARACTERISTIC_UUID,
+      BLECharacteristic::PROPERTY_READ |
+      BLECharacteristic::PROPERTY_NOTIFY
+  );
+
+  pCharacteristic->addDescriptor(new BLE2902());
+  pCharacteristic->setValue("0");
+  pService->start();
+
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x0);
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
+
 }
 
 void loop() {
